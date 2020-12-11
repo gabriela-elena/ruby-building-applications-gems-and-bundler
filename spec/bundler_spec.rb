311b4646b1619c5209c894f7a30840e60395203e
Gabriela Ruvalcaba <gabrielaelenadesigns@gmail.com>
2020-12-10 16:19:45 -0800

    commit tree e08347a8dd23debba84bafd07608e059f5b21031

--- spec/bundler_spec.rb
+++ spec/bundler_spec.rb
@@ -3,7 +3,7 @@ require 'bundler'
 describe "Bundler" do
   before :all do
     @bundle_output = ""
-    Bundler.with_unbundled_env do
+    Bundler.with_clean_env do
       @bundle_output = `bundle`
     end
   end
@@ -19,27 +19,27 @@ describe "Bundler" do
 
     # http://bundler.io/v1.3/gemfile.html
     it "should specify rubygems as a source using the SSL protocol on the first line" do
-      expect(@gemfile_text =~ /source ['"]https:\/\/rubygems\.org['"]/).not_to eq(nil)
+      expect(@gemfile_text =~ /source .https:\/\/rubygems.org./).not_to eq(nil)
     end
 
     # http://bundler.io/v1.3/gemfile.html
     it "should list the hashie gem without specifying a version" do
-      expect(@gemfile_text =~ /gem ['"]hashie['"]/).not_to eq(nil)
+      expect(@gemfile_text =~ /gem .hashie.\r\n?|\n|$/).not_to eq(nil)
     end
 
     # http://bundler.io/v1.3/gemfile.html
-    it "should list the sinatra gem with the specific version 2.0.2" do
-      expect(@gemfile_text =~ /gem ['"]sinatra['"], ?['"]v?2\.0\.2['"]/).not_to eq(nil)
+    it "should list the sinatra gem with the specific version 1.4.4" do
+      expect(@gemfile_text =~ /gem .sinatra., .v?1\.4\.4./).not_to eq(nil)
     end
 
     # http://robots.thoughtbot.com/post/2508037841/rubys-pessimistic-operator
     it "should list the octokit gem specifying version 2.0 with a twiddle-wakka" do
-      expect(@gemfile_text =~ /gem ['"]octokit['"], ?['"]~> ?2\.0['"]/).not_to eq(nil)
+      expect(@gemfile_text =~ /gem .octokit.*.~>.?2\.0./).not_to eq(nil)
     end
 
     # http://bundler.io/git.html
-    it "should list the awesome_print gem specifying a remote git repository using the SSH URL (use github)" do
-      expect(@gemfile_text =~ /gem ['"]awesome_print['"], ?(git:|:git ?=>) ?['"]git@github\.com:awesome\-print\/awesome_print\.git['"]/).not_to eq(nil)
+    it "should list the awesome_print gem specifying a remote git repository (use github)" do
+      expect(@gemfile_text =~ /gem .awesome_print.,.*git.*/).not_to eq(nil)
     end
 
     describe "groups" do
@@ -49,11 +49,11 @@ describe "Bundler" do
 
       # http://bundler.io/v1.3/groups.html
       it "should contain the pry gem in the development group using a hash argument to the gem method" do
-        expect(@gemfile_text).to  match(/gem ['"]pry['"], ?(group:|:group ?=>) ?(:development|['"]development['"])/) | match(/group ?(:development|['"]development['"])[\s\S]*gem ['"]pry['"][\s\S]*end/)
+        expect(@gemfile_text =~ /gem .pry.,.*group.*development'?/).not_to eq(nil)
         expect(@bundle_output =~ /pry/).not_to eq(nil)
 
         bundle_output_without_development = ""
-        Bundler.with_unbundled_env do
+        Bundler.with_clean_env do
           bundle_output_without_development = `bundle --without development`
         end
         expect(bundle_output_without_development =~ /pry/).to eq(nil)
@@ -61,11 +61,11 @@ describe "Bundler" do
 
       # http://bundler.io/v1.3/groups.html
       it "should contain the rspec gem in the test group using block syntax" do
-        expect(@gemfile_text =~ /group (:test|['"]test['"]) do/).not_to eq(nil)
+        expect(@gemfile_text =~ /group .*test.* do/).not_to eq(nil)
         expect(@bundle_output =~ /rspec/).not_to eq(nil)
 
         bundle_output_without_test = ""
-        Bundler.with_unbundled_env do
+        Bundler.with_clean_env do
           bundle_output_without_test = `bundle --without test`
         end
         expect(bundle_output_without_test =~ /rspec/).to eq(nil)
@@ -89,7 +89,7 @@ describe "Bundler" do
 
     # http://bundler.io/
     it "should require bundler/setup in the environment" do
-      expect(@environment_text =~ /require ['"]bundler\/setup['"]/).not_to eq(nil)
+      expect(@environment_text =~ /require .bundler\/setup./).not_to eq(nil)
     end
 
     # http://bundler.io/v1.3/groups.html
